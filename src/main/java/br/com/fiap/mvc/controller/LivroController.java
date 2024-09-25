bcb7339cb96407b78bc75da7d3eb80c43819f8cd
Phablo <126733018+PhabloFiap@users.noreply.github.com>
2024-09-24 22:44:32 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/mvc/controller/LivroController.java
@@ -0,0 +1,64 @@
+package br.com.fiap.mvc.controller;
+
+import br.com.fiap.mvc.model.Livro;
+import br.com.fiap.mvc.respository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("livro")
+public class LivroController {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @PostMapping("editar")
+    @Transactional
+    public String editar(Livro livro, Model model){
+        livroRepository.save(livro);
+        model.addAttribute("msg", "Livro atualizado");
+        model.addAttribute("books", livroRepository.findAll());
+        return "livro/lista";
+    }
+
+    @GetMapping("editar/{id}")
+    public String editar(@PathVariable("id") Long id, Model model){
+        //Pesquisar o livro pelo id e enviar o livro para a view
+        model.addAttribute("livro", livroRepository.findById(id));
+        //Retornar a view
+        return "livro/editar";
+    }
+
+    @GetMapping("listar") //localhost:8080/livro/listar
+    public String listar(Model model){
+        //Enviar a lista de livros para a view
+        model.addAttribute("books", livroRepository.findAll());
+        return "livro/lista";
+    }
+
+    @GetMapping("cadastrar")
+    public String cadastrar(Livro livro){
+        return "livro/cadastro";
+    }
+
+    @PostMapping("cadastrar")
+    @Transactional
+    public String cadastrar(Livro livro, Model model){
+        livroRepository.save(livro);
+        model.addAttribute("msg", "Livro registrado!");
+        return "livro/cadastro";
+    }
+    @PostMapping ("delete")
+    @Transactional
+    public String remover(Livro livro, Model model){
+        livroRepository.delete(livro);
+        model.addAttribute("msg", "Livro removido");
+        model.addAttribute("books", livroRepository.findAll());
+        return "redirect:/livro/listar";
+
+    }
+
+}
